3113529058f9299207cc3a51e6b4d998e7f6157d
Alexandre Nakamura <nakamura.ale@gmail.com>
2025-09-16 15:31:44 -0300

    ov

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,22 +8,40 @@ sap.ui.define([
         onInit() {
         },
 
-        onPress: function(){
-            
-            alert("Hello World");
+            onInit: function () {
+                //alert("onInit");
+            },
 
-        },
+            onBeforeRendering: function(){
+                //alert("onBeforeRendering");
+            },
 
-        onCalcular: function(){
-            var oView = this.getView();
-            var iB1   = parseInt(oView.byId("b1").getValue());
-            var iB2   = parseInt(oView.byId("b2").getValue());
-            var iB3   = parseInt(oView.byId("b3").getValue());
-            var iB4   = parseInt(oView.byId("b4").getValue());   
-            var fResultado = 0;
-            fResultado = ( iB1 + iB2 + iB3 + iB4) / 4;
-            
-            oView.byId("Resultado").setValue(fResultado);
-        }
+            onAfterRendering: function(){
+                //alert("onAfterRendering");
+            },
+
+            onExit: function(){
+                //alert("onExit");
+            },
+
+            onCalcular: function(){
+                var oView = this.getView();
+                var iB1 = parseInt(oView.byId("b1").getValue());
+                var iB2 = parseInt(oView.byId("b2").getValue());
+                var iB3 = parseInt(oView.byId("b3").getValue());
+                var iB4 = parseInt(oView.byId("b4").getValue());
+                var fResultado = 0;
+                
+                fResultado = ( iB1 + iB2 + iB3 + iB4) / 4;
+
+                oView.byId("resultado").setValue(fResultado);
+
+                MessageToast.show("Resultado = "+fResultado);
+
+                // MessageToast
+                // sap.m.MessageToast
+                
+                //oView.destroy();
+            }
     });
 });
